480a44fc683eba9cd058df7b7a4911227de3a6a6
Max Stone <stonemc@msoe.edu>
2026-09-24 14:15:41 -0500

    small change

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "Worlds")
     return f"Hello {name}!"
 
 if __name__ == "__main__":
